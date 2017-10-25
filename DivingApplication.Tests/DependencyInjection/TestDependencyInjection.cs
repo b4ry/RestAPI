635c3d85365bf1b77a0da2d9d9d17d3a6752da5d
b4ry <barry90@interia.pl>
2017-10-25 20:26:43 +1000

    Tests - database sets, di registration and resolving

--- DivingApplication.Tests/DependencyInjection/TestDependencyInjection.cs
+++ DivingApplication.Tests/DependencyInjection/TestDependencyInjection.cs
@@ -24,7 +24,7 @@ namespace DivingApplication.Tests.DependencyInjection
         }
 
         [Fact]
-        public void IUnitOfWorkMustBeResolvedProperlyWhenResolveIsCalled()
+        public void InversionOfControlContainerMustResolveUnitOfWorkWhenItIsCalledToDoSo()
         {
             IServiceCollection serviceCollection = new ServiceCollection();
             serviceCollection.AddDbContext<DivingApplicationDbContext>();
@@ -37,6 +37,35 @@ namespace DivingApplication.Tests.DependencyInjection
             IUnitOfWork resolvedComponent = container.Resolve<IUnitOfWork>();
 
             Assert.IsType(typeof(UnitOfWork), resolvedComponent);
+        }
+
+        [Fact]
+        public void DatabaseSetsMustBeRegisteredWhenInversionOfControlContainerIsRegistered()
+        {
+            IServiceCollection serviceCollection = new ServiceCollection();
+            IComponentRegistration componentRegistration = null;
+            var container = serviceCollection.AddApplicationModules();
+
+            container.ComponentRegistry.TryGetRegistration(new TypedService(typeof(IDatabaseSet)), out componentRegistration);
+            var resolvedComponentType = componentRegistration.Activator.LimitType.GetType();
+
+            Assert.Equal(componentRegistration.Activator.LimitType.FullName, typeof(DatabaseSet).FullName);
+        }
+
+        [Fact]
+        public void InversionOfControlContainerMustResolveDatabaseSetWhenItIsCalledToDoSo()
+        {
+            IServiceCollection serviceCollection = new ServiceCollection();
+            serviceCollection.AddDbContext<DivingApplicationDbContext>();
+            IContainer container = serviceCollection.AddApplicationModules();
+
+            var builder = new DbContextOptionsBuilder<DivingApplicationDbContext>().UseInMemoryDatabase();
+            var options = builder.Options;
+            var divingApplicationDbContext = new DivingApplicationDbContext(options);
+
+            IDatabaseSet resolvedComponent = container.Resolve<IDatabaseSet>();
+
+            Assert.IsType(typeof(DatabaseSet), resolvedComponent);
         }
     }
 }
