80ba44e7845ae2d01ef321587c2c8f646b772a19
b4ry <barry90@interia.pl>
2017-11-21 17:36:17 +1000

    - commands refactoring

---
+++ PortfolioApplication.Api/CQRS/Commands/CreateEntityCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Distributed;
+using PortfolioApplication.Entities.Entities;
+using PortfolioApplication.Services.DatabaseContext;
+
+namespace PortfolioApplication.Api.CQRS.Commands
+{
+    public class CreateEntityCommandHandler<TCommand, TEntity> : IHandleCommand<TCommand> 
+        where TCommand : ICommand
+        where TEntity : BaseEntity
+    {
+        private IUnitOfWork UnitOfWork { get; }
+        private IDatabaseSet DatabaseSet { get; }
+        private IDistributedCache RedisCache { get; }
+        private DbSet<TEntity> ExperienceSet { get; }
+
+        public CreateEntityCommandHandler(IDatabaseSet databaseSet, IUnitOfWork unitOfWork, IDistributedCache redisCache)
+        {
+            UnitOfWork = unitOfWork;
+            DatabaseSet = databaseSet;
+            ExperienceSet = DatabaseSet.Set<TEntity>();
+            RedisCache = redisCache;
+        }
+
+        public void Handle(TCommand command)
+        {
+            var experienceEntity = Mapper.Map<TEntity>(command);
+
+            ExperienceSet.Add(experienceEntity);
+
+            UnitOfWork.Save();
+            RedisCache.Remove(ComposeRedisKey(typeof(TEntity).Name, "*"));
+        }
+
+        private string ComposeRedisKey(string entityTypeName, string id)
+        {
+            return entityTypeName + ":" + id;
+        }
+    }
+}
